5f6bd6ade4ec920f4dd4c0277fba4dfd7588d84a
郝年俊 <haonj@wafa.com.cn>
2023-03-27 18:00:52 +0800

    增加.env文件

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require("path");
+
+function resolve(dir){
+  return path.join(__dirname,dir)
+}
+
+// 网页标题
+const name = process.env.VUE_APP_TITLE || '网页标题'
+
+// 端口
+const port = process.env.port || process.env.npm_config_port || 80 // 端口
+
 module.exports = defineConfig({
   transpileDependencies: true
 })
