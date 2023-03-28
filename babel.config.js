d0908e7cfb98728aa19a4b1a827c85c2dab354ec
郝年俊 <haonj@wafa.com.cn>
2023-03-28 15:44:05 +0800

    配置eslint babel

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ],
+  'env':{
+    'development':{
+      // babel-plugin-dynamic-import-node plugin 将所有的 import 转换问 require
+      // 当有大量页面时，可以显著提升热更新的速度
+      'plugins': ['dynamic-import-node']
+    }
+  }
 }
