6d6edf335504b2308ea4bd6b3554fb3e488cff9f
郝年俊 <haonj@wafa.com.cn>
2023-03-30 17:57:34 +0800

    TimePicker组件

--- src/components/ThemePicker/index.vue
+++ src/components/ThemePicker/index.vue
@@ -1,12 +1,19 @@
 <template>
-  <div>
-
-  </div>
+  <el-color-picker
+    v-model="theme"
+    :predefine="['#409EFF', '#1890ff', '#304156','#212121','#11a983', '#13c2c2', '#6959CD', '#f5222d', ]"
+    class="theme-picker"
+    popper-class="theme-picker-dropdown"
+  />
 </template>
 
 <script>
 export default {
-
+  data(){
+    return {
+      theme:''
+    }
+  }
 }
 </script>
 
